59e0dfd2d35225d26f22f269bef85c653b176904
ajxv <abin.xv@gmail.com>
2023-12-17 18:51:48 +0530

    added share option to entry details page

--- lib/screens/entry_details_screen.dart
+++ lib/screens/entry_details_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../services/mal_services.dart';
 import '../widgets/list_entry_card_widget.dart';
@@ -98,6 +99,18 @@ class _EntryDetailPageState extends State<EntryDetailPage> {
           onPressed: () => Navigator.of(context).pop(),
           icon: const Icon(Icons.chevron_left),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Share.share(
+                  "https://myanimelist.net/${widget.isAnime ? 'anime' : 'manga'}/${widget.entryId}");
+            },
+            icon: const Icon(
+              Icons.share,
+              size: 20,
+            ),
+          ),
+        ],
       ),
       body: FutureBuilder(
         future: _futureEntryInfo,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import flutter_secure_storage_macos
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
